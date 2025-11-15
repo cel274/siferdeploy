51353fc310e27bcfaeae4a4e1e31b6ed58df89b2
Lautaro Giordano <111036499+cel274@users.noreply.github.com>
2025-11-15 16:15:25 -0300

    asd

--- public/api/controllers/AuthController.php
+++ public/api/controllers/AuthController.php
@@ -1,6 +1,36 @@
 <?php
-require_once __DIR__ . '/../config/jwt.php';
 require_once __DIR__ . '/../models/Usuario.php';
+
+class JWT_Auth {
+    private static $secret_key = 'teamotomotakino';
+    
+    public static function encode($payload) {
+        $header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
+        $payload = json_encode($payload);
+        
+        $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+        $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+        
+        $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, self::$secret_key, true);
+        $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+        
+        return $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+    }
+    
+    public static function decode($jwt) {
+        $tokenParts = explode('.', $jwt);
+        if (count($tokenParts) !== 3) return false;
+        
+        list($header, $payload, $signature) = $tokenParts;
+        
+        $validSignature = hash_hmac('sha256', $header . "." . $payload, self::$secret_key, true);
+        $base64UrlValidSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($validSignature));
+        
+        if ($signature !== $base64UrlValidSignature) return false;
+        
+        return json_decode(base64_decode(str_replace(['-', '_'], ['+', '/'], $payload)), true);
+    }
+}
 
 class AuthController {
     private $usuarioModel;
@@ -31,7 +61,7 @@ class AuthController {
                     'exp' => time() + (24 * 60 * 60)
                 ];
                 
-                $token = JWT::encode($payload);
+                $token = JWT_Auth::encode($payload);
                 
                 echo json_encode([
                     'success' => true,
